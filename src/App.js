27013c923c8da336dafb62f9827e50f5383eac81
Aakarsh <aakarshs@outlook.com>
2019-04-03 16:17:15 -0400

    Update 2.0

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './Styles/App.css';
 import "./Styles/quizMaker.css";
-
+import AutosizeInput from 'react-input-autosize';
 
 class App extends Component {
   constructor(props) {
@@ -15,8 +15,9 @@ class App extends Component {
       newText: "",
       refresh: false,
       variableNumber: 0,
-      outputArray:'',
-      outputs: [],
+      outputArray: '',
+      outputs: [""],
+      selectedOutput: 0,
     };
 
     this.x = [];
@@ -55,39 +56,40 @@ class App extends Component {
 
   render() {
     return (
-      <div style={{ backgroundColor: "#272637", width: "100%", height: 900 }}>
-        <div style={{ paddingTop: 200, paddingLeft: 50 }}>
-
-          <div style={{ fontSize: 50, color: 'white' }}>QuizMaker</div>
+      <div style={{ backgroundColor: "#F3F6F8", width: "100%",  height:2000 }}>
+        <div id='mainContainer'>
+          <div>
+          <div id="QuizMakerTitle" >QuizMaker</div>
 
           <div style={{ display: 'flex', flexDirection: 'row' }}>
-            <div style={{ width: 600, height: 300, backgroundColor: "white", borderRadius: 20, padding: 20 }}>
+            <div>
               <div id="canvas">
                 {this.state.inputs.map((item, index) => {
                   //console.log("The current iteration is: " + index);
-                  if (item == "string") {
-                    return (<input id="textBox"
+                  if (item[0] == "a") {
+                    return (<input id="variables" value={item} onChange={this.onChangeAge.bind(this)} />)
+                  }
+                  else {
+                    return (<input  
                       onChange={(e) => {
                         this.x = this.state.inputs;
                         this.x[index] = e.target.value;
                         console.log("=====")
                         console.log(this.x)
                         console.log("=====")
+                        this.setState({refresh:!this.state.refresh})
                         // this.onChangeAge();
                       }
+                    
                       }
-
+                      
+                      style={{backgroundColor:'#E8E9EA', padding:9, borderWidth:0, borderRadius:10, width:this.state.inputs[index].length*6.5 }}
+                       value={this.state.inputs[index]}
 
                     />)
+                  }   
                   }
-                  else if (item[0] == "a") {
-                    return (<input style={{ borderRadius: 100, borderWidth: 0, margin: 10, backgroundColor: 'red', textAlign: 'center', width: 50, height: 50 }} value={item} onChange={this.onChangeAge.bind(this)} />)
-                  }
-                  else {
-                    return (<input id="textBox" onChange={this.onChangeAge.bind(this)} />)
-
-                  }
-                }
+              
                 )}
               </div>
             </div>
@@ -96,15 +98,15 @@ class App extends Component {
               <div style={{ display: 'flex', flexDirection: 'column' }}>
                 <button id="addVariable" onClick={() => this.appendToArray("sqrt")}> Add Variable </button>
 
-                <div style={{ fontSize: 30, color: 'white' }}> Math Functions </div>
+                <div id="mathTitle"> Math Functions </div>
                 <div style={{ width: 400, flexWrap: 'wrap' }}>
-                  <button id="mathButtons" onClick={() => this.setState({outputArray:this.state.outputArray+'Math.sqrt'})}> sqrt </button>
-                  <button id="mathButtons"  onClick={() => this.setState({outputArray:this.state.outputArray+'+'})}> add </button>
-                  <button id="mathButtons"  onClick={() => this.appendToArray("sqrt")}> sub </button>
-                  <button id="mathButtons"  onClick={() => this.appendToArray("sqrt")}> div </button>
-                  <button id="mathButtons"  onClick={() => this.appendToArray("sqrt")}> mul </button>
-                  <button id="mathButtons"  onClick={() => this.appendToArray("sqrt")}> mod </button>
-                  <button id="mathButtons"  onClick={() => console.log(this.x)}> array output </button>
+                  <button id="mathButtons" onClick={() => this.setState({ outputArray: this.state.outputArray + 'Math.sqrt' })}> sqrt </button>
+                  <button id="mathButtons" onClick={() => {this.state.outputs[this.state.selectedOutput] = this.state.outputs[this.state.selectedOutput] + '+' ; this.setState({refresh:!this.state.refresh}) }}> add </button>
+                  <button id="mathButtons" onClick={() => this.appendToArray("sqrt")}> sub </button>
+                  <button id="mathButtons" onClick={() => this.appendToArray("sqrt")}> div </button>
+                  <button id="mathButtons" onClick={() => this.appendToArray("sqrt")}> mul </button>
+                  <button id="mathButtons" onClick={() => this.appendToArray("sqrt")}> mod </button>
+                  <button id="mathButtons" onClick={() => console.log(this.x)}> array output </button>
 
                 </div>
               </div>
@@ -113,24 +115,13 @@ class App extends Component {
 
 
           <div>
-            <div style={{ fontSize: 20, color: 'white', marginBottom: 10, }}>Ouput</div>
-            <div style={{flexDirection:'row', display:'flex'}}>
-            <div style={{ width: 650, borderRadius: 10, height: 40, backgroundColor: 'white' }}>
-            {this.state.outputArray}
-            </div>
-            <div style={{backgroundColor:'green', color:'white', padding:10, borderRadius:15, marginLeft:10}}>Add Another output</div>
-            </div>
-          </div>
-
-
-          <div id="canvas">
             {this.state.inputs.map((item, index) => {
               //console.log("The current iteration is: " + index);
               if (item[0] == "a") {
-                return (<button style={{ borderRadius: 100, borderWidth: 0, margin: 10, backgroundColor: 'red', textAlign: 'center', width: 50, height: 50 }} 
-                onClick={()=>{this.setState({outputArray:this.state.outputArray+item})}}
-                onChange={this.onChangeAge.bind(this)}> 
-                {item}
+                return (<button id="outputVariables"  
+                  onClick={() => { this.state.outputs[this.state.selectedOutput] = this.state.outputs[this.state.selectedOutput] + item; this.setState({ refresh: !this.state.refresh }) }}
+                  onChange={this.onChangeAge.bind(this)}>
+                  {item}
                 </button>
                 )
               }
@@ -141,23 +132,24 @@ class App extends Component {
           </div>
 
           <div>
-            <div style={{ fontSize: 20, color: 'white', marginBottom: 10, }}>Ouput</div>
-            <div style={{flexDirection:'row', display:'flex'}}>
-            <div id="canvas">
-            {this.state.outputArray}
-            {this.state.outputs.map((item, index) => {
-              //console.log("The current iteration is: " + index);
-                return (  <input style={{ width: 650, borderRadius: 10, height: 40, backgroundColor: 'white' }}/>
-              )
-              })
-            }
-          </div>
-            <div onClick={()=> this.setState({ outputs: this.state.outputs.concat(["string"]) }) } style={{backgroundColor:'green', color:'white', padding:10, borderRadius:15, marginLeft:10}}>Add Another output</div>
+            <div id="outputTitle">Ouput</div>
+            <div style={{ flexDirection: 'row', display: 'flex' }}>
+              <div>
+                {/* Ouputs */}
+                {this.state.outputs.map((item, index) => {
+                  //console.log("The current iteration is: " + index);
+                  return (
+                  <div style={{flexDirection:'column', marginBottom:15,}}>
+                  <input id="outputCanvas" onClick={() => this.setState({ selectedOutput: index })} value={this.state.outputs[index]}  />
+                  </div>
+                  )})}
+              </div>
+              <div onClick={() => this.setState({ outputs: this.state.outputs.concat([""]) })} id="addOutput" >Add output</div>
             </div>
           </div>
 
 
-        
+          </div>
 
         </div>
       </div>
